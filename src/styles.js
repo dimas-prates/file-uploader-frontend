bacc12f7566dec2692ac19ad714a9676dd3b4df5
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-07-08 11:53:52 -0300

    :recycle: refactor(styles - index): Adjuste the height for percentage

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 export const Container = styled.div`
-  height: 100vh;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
